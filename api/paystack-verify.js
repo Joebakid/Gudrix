bfba392bc5a37d094e019c8938fdc3d06b4b5156
Joseph Bawo <josephbawo@gmail.com>
2026-02-21 10:08:53 +0100

    verfication bug

--- api/paystack-verify.js
+++ api/paystack-verify.js
@@ -1,11 +1,16 @@
-// api/paystack-verify.js
-
 export default async function handler(req, res) {
-  if (req.method !== "POST") {
-    return res.status(405).json({ success: false });
-  }
-
   try {
+    if (req.method !== "POST") {
+      return res.status(405).json({ success: false });
+    }
+
+    if (!process.env.PAYSTACK_SECRET_KEY) {
+      return res.status(500).json({
+        success: false,
+        error: "Secret key not configured",
+      });
+    }
+
     const { reference } = req.body;
 
     const verify = await fetch(
@@ -19,12 +24,13 @@ export default async function handler(req, res) {
 
     const data = await verify.json();
 
-    if (data.data.status !== "success") {
+    if (!data.status || data.data.status !== "success") {
       return res.status(400).json({ success: false });
     }
 
     return res.status(200).json({ success: true });
   } catch (err) {
+    console.error("VERIFY ERROR:", err);
     return res.status(500).json({ success: false });
   }
 }
